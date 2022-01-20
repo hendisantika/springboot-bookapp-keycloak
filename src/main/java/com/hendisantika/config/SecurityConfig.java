26ccb98d6af6f818cb1a76cb7b2a7da242632d55
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 14:00:17 +0700

    #24 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bookapp-keycloak
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/01/22
+ * Time: 13.59
+ * To change this template use File | Settings | File Templates.
+ */
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+}
