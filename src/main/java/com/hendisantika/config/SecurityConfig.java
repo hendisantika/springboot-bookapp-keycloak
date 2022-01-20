fa3b280b6daabb2e7ce211a52e78cbbea64ccaa6
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 14:03:33 +0700

    #25 - Add configureGlobal in SecurityConfig

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.hendisantika.config;
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,4 +19,27 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
  */
 @KeycloakConfiguration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     * <p>
+     * Since Spring Security requires that role names start with "ROLE_",
+     * a SimpleAuthorityMapper is used to instruct the KeycloakAuthenticationProvider
+     * to insert the "ROLE_" prefix.
+     * <p>
+     * e.g. Librarian -> ROLE_Librarian
+     * <p>
+     * Should you prefer to have the role all in uppercase, you can instruct
+     * the SimpleAuthorityMapper to convert it by calling:
+     * {@code grantedAuthorityMapper.setConvertToUpperCase(true); }.
+     * The result will be: Librarian -> ROLE_LIBRARIAN.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
+        grantedAuthorityMapper.setPrefix("ROLE_");
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(grantedAuthorityMapper);
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
 }
