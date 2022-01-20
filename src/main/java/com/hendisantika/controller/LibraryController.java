b3621eae7296c259b988fca4f9ff1fc9abc0c55c
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 13:47:39 +0700

    #13 - Add LibraryController

---
+++ src/main/java/com/hendisantika/controller/LibraryController.java
@@ -0,0 +1,29 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bookapp-keycloak
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/01/22
+ * Time: 13.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class LibraryController {
+
+    private final HttpServletRequest request;
+    private final BookRepository bookRepository;
+
+    public LibraryController(HttpServletRequest request, BookRepository bookRepository) {
+        this.request = request;
+        this.bookRepository = bookRepository;
+    }
+
+}
