f6fd670b319c0b7fd83146388b4db424ec8388ea
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 13:43:52 +0700

    #7 - Add readAll in BookRepository

--- src/main/java/com/hendisantika/repository/BookRepository.java
+++ src/main/java/com/hendisantika/repository/BookRepository.java
@@ -3,6 +3,9 @@ package com.hendisantika.repository;
 import com.hendisantika.model.Book;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -24,5 +27,11 @@ public class BookRepository {
         books.put("B01", new Book("B01", "Harry Potter and the Deathly Hallows", "J.K. Rowling"));
         books.put("B02", new Book("B02", "The Lord of the Rings", "J.R.R. Tolkien"));
         books.put("B03", new Book("B03", "Fantastic Beast", "J.K. Rowling"));
+    }
+
+    public List<Book> readAll() {
+        List<Book> allBooks = new ArrayList<>(books.values());
+        allBooks.sort(Comparator.comparing(Book::getId));
+        return allBooks;
     }
 }
