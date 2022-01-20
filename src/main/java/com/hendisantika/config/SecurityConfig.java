28eb328e14a79e6171bae2947151e8010a5246f7
Hendi Santika <hendisantika@yahoo.co.id>
2022-01-20 14:04:34 +0700

    #28 - Add configure in SecurityConfig

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -72,5 +73,18 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @ConditionalOnMissingBean(HttpSessionManager.class)
     protected HttpSessionManager httpSessionManager() {
         return new HttpSessionManager();
+    }
+
+    /**
+     * Define security constraints for the application resources.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/books").hasAnyRole("Member", "Librarian")
+                .antMatchers("/manager").hasRole("Librarian")
+                .anyRequest().permitAll();
     }
 }
