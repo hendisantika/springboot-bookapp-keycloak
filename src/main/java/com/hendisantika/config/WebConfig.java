c79d847f380b61fdcf64cc2645b001a7e851eb4c
Hendi Santika <hendisantika@yahoo.co.id>
2022-05-18 09:24:06 +0700

    #35 - Add WebConfig

---
+++ src/main/java/com/hendisantika/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-bookapp-keycloak
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/05/22
+ * Time: 14.12
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                        "/webjars/**",
+                        "/images/**",
+                        "/css/**",
+                        "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/images/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+
+}
